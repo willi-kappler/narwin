a569c4bfa19930aa965eab9c06976b93adde20b8
Willi Kappler <willi.kappler.gm@gmail.com>
2024-01-01 17:10:13 +0100

    Add different mutation operation

--- examples/sudoku/sudoku_individual.nim
+++ examples/sudoku/sudoku_individual.nim
@@ -80,13 +80,18 @@ method naMutate*(self: var SudokuIndividual) =
     var i = randIndex()
     var j = randIndex()
 
+    # All missing numbers:
+    var numbers: seq[uint8] = @[]
+    # All numbers that are in use:
+    var inUse: set[uint8]
+
     # Ensure that the value is allowed to change:
     while self.getVal1(i, j) != 0:
         i = randIndex()
         j = randIndex()
 
     # Select a random operation:
-    let operation = rand(1)
+    let operation = rand(4)
 
     case operation
     of 0:
@@ -118,23 +123,83 @@ method naMutate*(self: var SudokuIndividual) =
             i1 = i - 1
             i2 = i + 1
 
-
-        var v = randValue()
+        # Top:
         let v1 = self.getVal2(i, j1)
+        # Bottom:
         let v2 = self.getVal2(i, j2)
+        # Left:
         let v3 = self.getVal2(i1, j)
+        # Right:
         let v4 = self.getVal2(i2, j)
 
-        while true:
-            if (v1 != v) and (v2 != v) and (v3 != v) and (v4 != v):
-                break
+        for n in 1'u8..9'u8:
+            if (n != v1) and (n != v2) and (n != v3) and (n != v4):
+                numbers.add(n)
 
-            v = randValue()
+        let k = rand(numbers.high)
+        self.setVal2(i, j, numbers[k])
+    of 2:
+        # Check row:
+        for k in 0'u8..8'u8:
+            let n = self.getVal1(k, i)
+            if n > 0:
+                inUse.incl(n)
 
-        self.setVal2(i, j, v)
+        for n in 1'u8..9'u8:
+            if n notin inUse:
+                numbers.add(n)
+
+        shuffle(numbers)
+        var l = 0
+
+        for k in 0'u8..8'u8:
+            if self.getVal1(k, i) == 0:
+                self.setVal2(k, i, numbers[l])
+                inc(l)
+    of 3:
+        # Check col:
+        for k in 0'u8..8'u8:
+            let n = self.getVal1(i, k)
+            if n > 0:
+                inUse.incl(n)
+
+        for n in 1'u8..9'u8:
+            if n notin inUse:
+                numbers.add(n)
+
+        shuffle(numbers)
+        var l = 0
+
+        for k in 0'u8..8'u8:
+            if self.getVal1(i, k) == 0:
+                self.setVal2(i, k, numbers[l])
+                inc(l)
+    of 4:
+        # Check block:
+        i = uint8(rand(2) * 3)
+        j = uint8(rand(2) * 3)
+
+        for u in 0'u8..2'u8:
+            for v in 0'u8..2'u8:
+                let n = self.getVal1(i + u, j + v)
+                if n > 0:
+                    inUse.incl(n)
+
+        for n in 1'u8..9'u8:
+            if n notin inUse:
+                numbers.add(n)
+
+        shuffle(numbers)
+        var l = 0
+
+        for u in 0'u8..2'u8:
+            for v in 0'u8..2'u8:
+                if self.getVal1(i + u, j + v) == 0:
+                    self.setVal2(i + u, j + v, numbers[l])
+                    inc(l)
+
     else:
         raise newException(ValueError, fmt("Unknown mutation operation: {operation}"))
-
 
 method naRandomize*(self: var SudokuIndividual) =
     let last = self.data1.high
@@ -152,11 +217,6 @@ method naCalculateFitness*(self: var SudokuIndividual) =
         for n in 0'u8..9'u8:
             errors += self.checkLineN(0, i, 1, 0, n)
             errors += self.checkLineN(i, 0, 0, 1, n)
-
-    # Check diagonals:
-    #for n in 0'u8..9'u8:
-        #errors += self.checkLineN(0, 0, 1, 1, n)
-        #errors += self.checkLineN(8, 0, -1, 1, n)
 
     # Check each block:
     for i in countup(0'u8, 6'u8, 3'u8):
@@ -180,17 +240,17 @@ method naToJSON*(self: SudokuIndividual): JsonNode =
 
 proc newPuzzle*(): SudokuIndividual =
     let data: seq[uint8] = @[
-        0, 0, 0,   0, 0, 0,   0, 0, 0,
-        0, 0, 0,   0, 0, 0,   0, 0, 0,
-        0, 0, 0,   0, 0, 0,   0, 0, 0,
+        0, 3, 0,   0, 0, 0,   0, 0, 0,
+        0, 0, 0,   1, 9, 5,   0, 0, 0,
+        0, 0, 8,   0, 0, 0,   0, 6, 0,
 
-        0, 0, 0,   0, 0, 0,   0, 0, 0,
-        0, 0, 0,   0, 0, 0,   0, 0, 0,
-        0, 0, 0,   0, 0, 0,   0, 0, 0,
+        8, 0, 0,   0, 6, 0,   0, 0, 0,
+        4, 0, 0,   8, 0, 0,   0, 0, 1,
+        0, 0, 0,   0, 2, 0,   0, 0, 0,
 
-        0, 0, 0,   0, 0, 0,   0, 0, 0,
-        0, 0, 0,   0, 0, 0,   0, 0, 0,
-        0, 0, 0,   0, 0, 0,   0, 0, 0
+        0, 6, 0,   0, 0, 0,   2, 8, 0,
+        0, 0, 0,   4, 1, 9,   0, 0, 5,
+        0, 0, 0,   0, 0, 0,   0, 7, 0
         ]
 
     result = SudokuIndividual(data1: data, data2: data)
