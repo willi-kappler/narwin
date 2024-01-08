ada01de0eab8eea215e465a4d02ab5910b2fda60
Willi Kappler <willi.kappler.gm@gmail.com>
2024-01-08 11:14:54 +0100

    Refactor

--- src/narwin/na_population_node6.nim
+++ src/narwin/na_population_node6.nim
@@ -9,6 +9,7 @@
 
 # Nim std imports
 from std/strformat import fmt
+from std/random import rand
 
 # External imports
 import num_crunch
@@ -21,27 +22,28 @@ import na_population
 type
     NAPopulationNodeDP6 = ref object of NCNodeDataProcessor
         population: NAPopulation
+        bestFitness: float64
 
 method ncProcessData(self: var NAPopulationNodeDP6, inputData: seq[byte]): seq[byte] =
     ncDebug("ncProcessData()", 2)
 
     var tmpIndividual = self.population.naClone(0)
-    var bestFitness = tmpIndividual.fitness
     let populationSize = float64(self.population.populationSize)
+    let limitFactor = rand(4.0) + 1.0
 
     self.population.naResetOrAcepptBest(inputData)
 
     block iterations:
         for i in 0..<self.population.numOfIterations:
             let numberOfMutations = self.population.naGetNumberOfMutations()
+
             for j in 0..<self.population.populationSize:
-                let fitnessLimit = bestFitness * (1.0 + (float64(j) * 4.0 / populationSize))
+                let fitnessLimit = self.bestFitness * (1.0 + (float64(j) * limitFactor / populationSize))
                 tmpIndividual = self.population.naClone(j)
 
-                # And mutate it:
                 for _ in 0..<numberOfMutations:
                     tmpIndividual.naMutate()
-                # Calculate the new fitness for the mutated individual:
+
                 tmpIndividual.naCalculateFitness()
 
                 if tmpIndividual.fitness < fitnessLimit:
@@ -53,11 +55,10 @@ method ncProcessData(self: var NAPopulationNodeDP6, inputData: seq[byte]): seq[b
                     ncDebug(fmt("Early exit at i: {i}"))
                     break iterations
 
-                if tmpIndividual.fitness < bestFitness:
-                    bestFitness = tmpIndividual.fitness
-                    # Maybe clone it to the first position: self.population[0]
-                    # if j > 0:
-                    #   self.population[j] = tmpIndividual.naClone()
+                if tmpIndividual.fitness < self.bestFitness:
+                    self.bestFitness = tmpIndividual.fitness
+                    if j > 0:
+                        self.population[j] = tmpIndividual.naClone()
 
     # Find the best and the worst individual at the end:
     self.population.findBestAndWorstIndividual()
@@ -74,6 +75,7 @@ proc naInitPopulationNodeDP6*(individual: NAIndividual, config: NAConfiguration)
     result = NAPopulationNodeDP6(population: population)
     result.population[0] = individual.naClone()
     result.population[0].naCalculateFitness()
+    result.bestFitness = result.population[0].fitness
 
     # Initialize the population with random individuals:
     for i in 1..<config.populationSize:
