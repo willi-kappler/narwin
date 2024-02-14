451d3eef01e239d85142f4da5d9142f6aa3e202a
Willi Kappler <willi.kappler@uni-tuebingen.de>
2024-02-14 18:06:44 +0100

    Optimize row and col at the same time

--- examples/sudoku/sudoku_individual.nim
+++ examples/sudoku/sudoku_individual.nim
@@ -94,157 +94,64 @@ proc randomValue(): uint8 =
 proc randomIndex(): uint8 =
     uint8(rand(8))
 
-proc decIndex(i: uint8): uint8 =
-    if i == 0:
-        return 8
-    else:
-        return i - 1
-
-proc incIndex(i: uint8): uint8 =
-    if i == 8:
-        return 0
-    else:
-        return i + 1
-
-proc randomEmptyPosition1(self: SudokuIndividual): (uint8, uint8) =
-    var col = randomIndex()
-    var row = randomIndex()
-
-    while self.getValue1(col, row) != 0:
-        col = randomIndex()
-        row = randomIndex()
-
-    return (col, row)
-
-proc randomValue2(self: SudokuIndividual, col, row: uint8): uint8 =
-    let c1 = decIndex(col)
-    let c2 = incIndex(col)
-    let r1 = decIndex(row)
-    let r2 = incIndex(row)
-
-    let v1 = self.getValue2(col, r1)
-    let v2 = self.getValue2(col, r2)
-    let v3 = self.getValue2(c1, row)
-    let v4 = self.getValue2(c2, row)
-
-    result = randomValue()
-
-    while (result == v1) or (result == v2) or (result == v3) or (result == v4):
-        result = randomValue()
-
-proc swapValues(self: var SudokuIndividual, col1, row1, col2, row2: uint8) =
-    if (col1 == col2) and (row1 == row2):
-        return
-
-    let v1 = self.getValue2(col1, row1)
-    let v2 = self.getValue2(col2, row2)
-
-    self.setValue2(col1, row1, v2)
-    self.setValue2(col2, row2, v1)
-
-proc incValue(self: var SudokuIndividual, col, row: uint8) =
-    let n = self.getValue2(col, row)
-
-    if n == 9:
-        self.setValue2(col, row, 1)
-    else:
-        self.setValue2(col, row, n + 1)
-
-proc decValue(self: var SudokuIndividual, col, row: uint8) =
-    let n = self.getValue2(col, row)
-
-    if n <= 1:
-        self.setValue2(col, row, 9)
-    else:
-        self.setValue2(col, row, n - 1)
-
-proc swapInCol(self: var SudokuIndividual, col, row1: uint8) =
-    var row2 = randomIndex()
-
-    while self.getValue1(col, row2) != 0:
-        row2 = randomIndex()
-
-    self.swapValues(col, row1, col, row2)
-
-proc swapInRow(self: var SudokuIndividual, col1, row: uint8) =
-    var col2 = randomIndex()
-
-    while self.getValue1(col2, row) != 0:
-        col2 = randomIndex()
-
-    self.swapValues(col1, row, col2, row)
-
-proc permutateValues(self: var SudokuIndividual, col1, row1: uint8) =
-    var (col2, row2) = self.randomEmptyPosition1()
-
-    while (col1 == col2) and (row1 == row2):
-        (col2, row2) = self.randomEmptyPosition1()
-
-    var bestValue1 = self.getValue2(col1, row1)
-    var bestValue2 = self.getValue2(col2, row2)
-    var bestFitness = self.calculateFitness2()
-
-    for i in 1u8..9:
-        for j in 1u8..9:
-            self.setValue2(col1, row1, i)
-            self.setValue2(col2, row2, j)
-
-            let fitness = self.calculateFitness2()
-
-            if fitness < bestFitness:
-                bestFitness = fitness
-                bestValue1 = i
-                bestValue2 = j
-
-    self.setValue2(col1, row1, bestValue1)
-    self.setValue2(col2, row2, bestValue2)
-
-proc randomizeCol(self: var SudokuIndividual, col: uint8) =
-    for row in 0u8..8:
-        if self.getValue1(col, row) == 0:
-            self.setValue2(col, row, randomValue())
-
-proc randomizeRow(self: var SudokuIndividual, row: uint8) =
-    for col in 0u8..8:
-        if self.getValue1(col, row) == 0:
-            self.setValue2(col, row, randomValue())
-
 method naMutate*(self: var SudokuIndividual) =
-    let (col, row) = self.randomEmptyPosition1()
+    let col = randomIndex()
+    let row = randomIndex()
 
-    var probability = 100
-    let operation = 1
+    var colInUse: set[uint8]
+    var rowInUse: set[uint8]
+    var j: uint8
+    var n: uint8
 
-    case operation
-    of 0:
-        self.setValue2(col, row, randomValue())
-    of 1:
-        self.setValue2(col, row, self.randomValue2(col, row))
-    of 2:
-        self.incValue(col, row)
-    of 3:
-        self.decValue(col, row)
-    of 4:
-        self.swapInCol(col, row)
-    of 5:
-        self.swapInRow(col, row)
-    of 6:
-        if rand(probability) == 0:
-            self.permutateValues(col, row)
-        else:
-            self.setValue2(col, row, randomValue())
-    of 7:
-        self.randomizeCol(col)
-    of 8:
-        self.randomizeRow(row)
-    of 9:
-        self.randomizeCol(col)
-        self.randomizeRow(row)
-    of 10:
-        let (col2, row2) = self.randomEmptyPosition1()
-        self.swapValues(col, row, col2, row2)
-    else:
-        raise newException(ValueError, fmt("Unknown mutation operation: {operation}"))
+    for i in 0'u8..8:
+        let nc = self.getValue1(col, i)
+        let nr = self.getValue1(i, row)
+
+        if nc > 0:
+            colInUse.incl(nc)
+        if nr > 0:
+            rowInUse.incl(nr)
+
+    var numbers: seq[uint8] = @[1, 2, 3, 4, 5, 6, 7, 8, 9]
+    shuffle(numbers)
+
+    if self.getValue1(col, row) == 0:
+        # Set value in (col, row)
+        for n in numbers:
+            if n in colInUse:
+                continue
+            if n in rowInUse:
+                continue
+            self.setValue2(col, row, n)
+            colInUse.incl(n)
+            rowInUse.incl(n)
+            break
+
+    # Set values in col:
+    shuffle(numbers)
+    j = 0
+    for i in 0'u8..8:
+        if i != row:
+            if self.getValue1(col, i) == 0:
+                n = numbers[j]
+                while n in colInUse:
+                    inc(j)
+                    n = numbers[j]
+                self.setValue2(col, i, n)
+                inc(j)
+
+    # Set values in row:
+    shuffle(numbers)
+    j = 0
+    for i in 0'u8..8:
+        if i != col:
+            if self.getValue1(i, row) == 0:
+                n = numbers[j]
+                while n in rowInUse:
+                    inc(j)
+                    n = numbers[j]
+                self.setValue2(i, row, n)
+                inc(j)
 
 method naRandomize*(self: var SudokuIndividual) =
     # Initialize with random values:
