be8cb1f22b1018a60c6f03017dc500b5cd525d7e
Willi Kappler <willi.kappler@uni-tuebingen.de>
2024-03-01 13:57:36 +0100

    Finish first test case, add more setup code

--- tests/test_na_population.nim
+++ tests/test_na_population.nim
@@ -15,11 +15,34 @@ type
     TestIndividual* = ref object of NAIndividual
         data: string
 
+    FakeRandom = object
+        i: uint32
+        values: seq[string]
+
+var fakeStrings = FakeRandom(i: 0, values: @[
+        "v22",
+        "v1",
+        "v999999999",
+        "v333",
+        "v666666",
+        "v7777777",
+        "v55555",
+        "v88888888",
+        "vvvvvvvvvvv",
+        "v4444"
+    ])
+
+proc nextRandomValue(): string =
+    result = fakeStrings.values[fakeStrings.i]
+    inc(fakeStrings.i)
+    if fakeStrings.i > uint32(fakeStrings.values.high):
+        fakeStrings.i = 0
+
 method naMutate*(self: var TestIndividual) =
-    self.data = "Mutate2"
+    self.data = nextRandomValue()
 
 method naRandomize*(self: var TestIndividual) =
-    self.data = "Randomize2"
+    self.data = nextRandomValue()
 
 method naCalculateFitness*(self: var TestIndividual) =
     self.fitness = float64(self.data.len())
@@ -40,9 +63,43 @@ method naToJSON*(self: TestIndividual): JsonNode =
 method naFromJSON*(self: TestIndividual, data: JsonNode): NAIndividual =
     return data.jsonTo(TestIndividual)
 
-proc test1() =
-    discard
+proc makeConfig: NAConfiguration =
+    result.serverMode = false
+    result.targetFitness = 0.0
+    result.resultFilename = ""
+    result.saveNewFitness = true
+    result.sameFitness = false
+    result.shareOnyBest = false
+
+    result.populationSize = 5
+    result.numOfIterations = 10
+    result.numOfMutations = 2
+    result.acceptNewBest = true
+    result.resetPopulation = false
+    result.populationKind = 1
+
+    result.dt = 0.001
+    result.amplitude = 1.0
+    result.base = 1.0
+    result.limitFactor = 1.01
+
+    result.loadIndividual = ""
+
+proc test1_init() =
+    let config1 = makeConfig()
+    var individual1 = TestIndividual(data: "Test1")
+    individual1.fitness = 5
+    let initPopulation = newSeq[NAIndividual](config1.populationSize)
+    let population = naInitPopulation(individual1, config1, initPopulation)
+
+    assertValues(population.population[0], "v1", 2.0)
+    assertValues(population.population[1], "v22", 3.0)
+    assertValues(population.population[2], "v333", 4.0)
+    assertValues(population.population[3], "Test1", 5.0)
+    assertValues(population.population[4], "v999999999", 10.0)
+
+    assert(uint32(population.population.len()) == config1.populationSize)
 
 when isMainModule:
-    test1()
+    test1_init()
 
